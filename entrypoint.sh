3bda20a00e55e7939941e203e2f17769532fc327
vajonam <152501+vajonam@users.noreply.github.com>
2024-04-03 13:19:11 -0400

    fix: add VGL_DISPLAY param:
    Signed-off-by: vajonam <152501+vajonam@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,5 +18,7 @@ fi
 export LOCALFBPORT=$((${VNC_PORT} + DISPLAY_NUMBER))
 export VGLRUN="${VGLRUN:-/usr/bin/vglrun}"
 export SUPD_LOGLEVEL="${SUPD_LOGLEVEL:-TRACE}"
+export VGL_DISPLAY="${VGL_DISPLAY:-egl}"
+
 # fix perms and launch supervisor with the above environment variables
 chown -R slic3r:slic3r /home/slic3r/ /configs/ /prints/ /dev/stdout && exec gosu slic3r supervisord -e $SUPD_LOGLEVEL
