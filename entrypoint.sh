7fde29ba73653d875b97779b806d5bbdc6746eb7
vajonam <152501+vajonam@users.noreply.github.com>
2024-04-03 09:36:34 -0400

    fix: add support for no security and full password sec.
    Signed-off-by: vajonam <152501+vajonam@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,10 @@ if [ -n "$VNC_PASSWORD" ]; then
   mkdir -p /root/.vnc
   echo "$VNC_PASSWORD" | vncpasswd -f > /root/.vnc/passwd
   chmod 0600 /root/.vnc/passwd
+  export VNC_SEC=
+else
+  export VNC_SEC="-securitytypes TLSNone,X509None,None"
 fi
-# vncserver "$DISPLAY" -securitytypes TLSNone,X509None,None -depth 24 -geometry "$VNC_RESOLUTION"
-# websockify -D --web=/usr/share/novnc/ "$NOVNC_PORT" localhost:$((5900 + DISPLAY_NUMBER))
-# echo "NoVNC server started on port $NOVNC_PORT"
-# EXEC=$@
-# exec "$@"
 export LOCALFBPORT=$((5900 + DISPLAY_NUMBER))
 export VGLRUN=vglrun
 chown -R slic3r:slic3r /home/slic3r/ /configs/ /prints/ /dev/stdout && exec gosu slic3r supervisord # -e TRACE
