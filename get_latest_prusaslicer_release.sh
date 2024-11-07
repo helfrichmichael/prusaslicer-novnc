5527ee9e2eef42714228ae8a6680cc11a68666bc
vajonam <152501+vajonam@users.noreply.github.com>
2024-11-07 10:11:56 -0500

    chore: update for changed naming convention for prusaslicer

--- get_latest_prusaslicer_release.sh
+++ get_latest_prusaslicer_release.sh
@@ -5,8 +5,8 @@ TMPDIR="$(mktemp -d)"
 curl -SsL https://api.github.com/repos/prusa3d/PrusaSlicer/releases/latest > $TMPDIR/latest.json
 
 # Grabs the first item of the latest result of the AppImage.
-url=$(jq -r '.assets[0] | select(.browser_download_url|test("linux-x64-GTK3.+.AppImage$"))| .browser_download_url' $TMPDIR/latest.json)
-name=$(jq -r '.assets[0] | select(.browser_download_url|test("linux-x64-GTK3.+.AppImage$"))| .name' $TMPDIR/latest.json)
+url=$(jq -r '.assets[] | select(.browser_download_url|test("linux-x64-older-distros-GTK3.*AppImage$"))|.browser_download_url' $TMPDIR/latest.json)
+name=$(jq -r '.assets[] | select(.browser_download_url|test("linux-x64-older-distros-GTK3.*AppImage$"))|.name' $TMPDIR/latest.json)
 version=$(jq -r .tag_name $TMPDIR/latest.json)
 
 if [ $# -ne 1 ]; then
